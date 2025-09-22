d480e5799a7c361d3eb4c8c3625b4beefc130730
Al-Hasib <mdabdullahalhasib1111@gmail.com>
2025-09-21 23:05:02 -0700

    update skeleton

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Conversational-AI-SaaS
+
+### RUN Application
+```python
+docker build -t fastapi-app .
+docker run -d -p 8000:8000 fastapi-app
+```
--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def say_hello(name: str):
     return {"message": f"Hello, {name}!"}
 
 
+@app.post("/chat")
+async def chat(query:str):
+    return {"message": "Thank You."}
+
 # Run the app directly with python main.py
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
